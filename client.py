6d5edb1c1ea6979139b971d44695e717c3bc71d6
jump85 <massafragiovanni@gmail.com>
2025-11-13 12:04:29 +0100

    Implement audio recording in client.py
    Added audio recording functionality with PyAudio.

--- client.py
+++ client.py
@@ -4,8 +4,16 @@ import time
 import board
 import neopixel
 import json
+import pyaudio
+import wave
+import time
 
 # Configurazione
+RATE = 44100
+CHUNK = 1024
+CHANNELS = 2
+FORMAT = pyaudio.paInt16
+
 NUM_LEDS = 10
 LED_PIN = board.D18
 UDP_PORT = 5005
@@ -16,6 +24,21 @@ pixels = neopixel.NeoPixel(LED_PIN, NUM_LEDS, brightness=0.5, auto_write=False)
 click = sa.WaveObject.from_wave_file("click.wav")
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 sock.bind(("0.0.0.0", UDP_PORT))
+
+# Setup audio record
+audio = pyaudio.PyAudio()
+stream = audio.open(
+    format=FORMAT,
+    channels=CHANNELS,
+    rate=RATE,
+    input=True,
+    frames_per_buffer=CHUNK,
+    input_device_index=1   # Sabrent
+)
+timestamp = time.strftime("%Y%m%d_%H%M%S")
+filename = f"recording_{timestamp}.wav"
+
+frames = []
 
 def flash_leds():
     pixels.fill((0, 255, 0))
@@ -40,5 +63,19 @@ while True:
             print(f"Beat! ({time.time():.3f})")
             click.play()
             flash_leds()
+        if msg.get("command") == "RECORD_START":
+            data = stream.read(CHUNK)
+            frames.append(data)
+        if msg.get("command") == "RECORD_STOP":
+            stream.stop_stream()
+            stream.close()
+            audio.terminate()
+
+            wf = wave.open(filename, 'wb')
+            wf.setnchannels(CHANNELS)
+            wf.setsampwidth(audio.get_sample_size(FORMAT))
+            wf.setframerate(RATE)
+            wf.writeframes(b''.join(frames))
+            wf.close()
     except Exception as e:
         print(f"Errore nella ricezione: {e}")
